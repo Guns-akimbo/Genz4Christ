b2da9a37d663d9ed4f8f778932d71020e5d3ec55
jnr_001 <obinyanemmanueljunior@gmail.com>
2025-09-29 17:09:15 +0100

    overlay update 4

--- src/components/herosection.tsx
+++ src/components/herosection.tsx
@@ -6,15 +6,15 @@ const HeroSection = () => {
         
         {/* Orange Overlay with Text */}
         <div className="bg-orange-500/80 text-white pl-20 pt-10 pb-10 pr-10 md:p-12 rounded-lg max-w-2xl h-[70vh]">
-          <h1 className="text-2xl md:text-3xl font-bold mb-4">
+          <h1 className="text-3xl md:text-3xl font-bold mb-10">
             Be the Gen-Z that will change the world
           </h1>
-          <p className="text-base md:text-lg mb-6">
+          <p className="text-base md:text-lg mb-10">
             Join a Global movement of young people unashamed of the gospel, driven by a passion to know God and make God known in their lives, career, & across the nations.
           </p>
           <a
             href="#"
-            className="underline font-semibold hover:text-gray-200 transition"
+            className="underline text-xl font-semibold hover:text-gray-200 transition"
           >
             Find Out More
           </a>
