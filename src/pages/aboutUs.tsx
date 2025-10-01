b9ee4b06268953abeae891e26f7bb8addca014b4
jnr_001 <obinyanemmanueljunior@gmail.com>
2025-10-01 22:18:55 +0100

    About page update

--- src/pages/aboutUs.tsx
+++ src/pages/aboutUs.tsx
@@ -1,8 +1,13 @@
+import Header from "../components/Header";
+import HeroSection from "../components/herosection";
+import Footer from "../components/footer";
 
 const AboutUs = () => {
   return (
     <div>
-      abouUs
+      <Header />
+      <HeroSection />
+      <Footer />
     </div>
   )
 }
