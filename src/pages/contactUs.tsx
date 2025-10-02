23c6c97f0714bace9c96c618151ebd58c382a92c
jnr_001 <obinyanemmanueljunior@gmail.com>
2025-10-02 11:29:06 +0100

    contact page dev

---
+++ src/pages/contactUs.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+
+import Footer from "../components/footer";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Header />
+      
+      <Footer />
+    </div>
+  )
+}
+
+export default AboutUs
